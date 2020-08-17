890ec481a452c2ed6478b23064cd106c716f0961
John Lonetree <jlonetree@gmail.com>
2020-08-17 00:16:15 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -28,7 +28,7 @@ def get_english_meaning(emote_file, jpn_emote)
   final_hash.each do |eng_meaning, value|
     if jpn_emote == value[:japanese]
       return eng_meaning
-    elsif 
+    elsif !jpn_emote == value[:japanese]
       return "Sorry, that emoticon was not found"
     end
   end
